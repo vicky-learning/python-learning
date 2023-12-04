0728b37e8440478777c8e8e936baeddc2354e9c4
vikashkumar0101 <vickyuu049@gmail.com>
2023-12-04 05:54:16 +0530

    add main file

---
+++ main.py
@@ -0,0 +1,4 @@
+# complete https://youtu.be/eWRfhZUzrAc
+# FastAPI - Python Framework - https://youtu.be/tLKKmouUams
+# Postgresql -  https://youtu.be/SpfIwlAYaKk
+# Git & Github - https://youtu.be/RGOj5yH7evk
